320176266930620e9696e241810ce4e75f1395a3
Samuel Peña <samuelpena098@gmail.com>
2025-01-06 00:02:55 -0400

    feat: add a sample static content

--- src/main/kotlin/com/sapp/Routing.kt
+++ src/main/kotlin/com/sapp/Routing.kt
@@ -1,11 +1,14 @@
 package com.sapp
 
 import io.ktor.server.application.*
+import io.ktor.server.http.content.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
 
 fun Application.configureRouting() {
     routing {
+        staticResources("/content", "mycontent")
+
         get("/") {
             call.respondText("Hello World!")
         }
