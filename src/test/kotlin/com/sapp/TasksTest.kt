7388b2efb4612efafe5757ed47663fce4388bd60
Samuel Peña <samuelpena098@gmail.com>
2025-01-07 10:15:48 -0400

    test: add tests to tasks

---
+++ src/test/kotlin/com/sapp/TasksTest.kt
@@ -0,0 +1,45 @@
+package com.sapp
+
+import io.ktor.client.request.*
+import io.ktor.client.statement.*
+import io.ktor.http.*
+import io.ktor.server.testing.*
+import junit.framework.TestCase.assertEquals
+import kotlin.test.Test
+import kotlin.test.assertContains
+
+class TasksTest {
+    @Test
+    fun tasksCanBeFoundByPriority() = testApplication {
+        application {
+            module()
+        }
+
+        val response = client.get("/tasks/byPriority/Medium")
+        val body = response.bodyAsText()
+
+        assertEquals(HttpStatusCode.OK, response.status)
+        assertContains(body, "Mow the lawn")
+        assertContains(body, "Paint the fence")
+    }
+
+    @Test
+    fun invalidPriorityProduces400() = testApplication {
+        application {
+            module()
+        }
+
+        val response = client.get("/tasks/byPriority/Invalid")
+        assertEquals(HttpStatusCode.BadRequest, response.status)
+    }
+
+    @Test
+    fun unusedPriorityProduces404() = testApplication {
+        application {
+            module()
+        }
+
+        val response = client.get("/tasks/byPriority/Vital")
+        assertEquals(HttpStatusCode.NotFound, response.status)
+    }
+}
