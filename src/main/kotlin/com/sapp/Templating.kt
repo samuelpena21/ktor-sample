afe3b1275bbeea2924c954bd6c355483b9770ace
Samuel Peña <samuelpena098@gmail.com>
2025-01-10 06:22:49 -0400

    feat: add thymeleaf route for tasks and a piece of code to return all task from template

--- src/main/kotlin/com/sapp/Templating.kt
+++ src/main/kotlin/com/sapp/Templating.kt
@@ -1,5 +1,7 @@
 package com.sapp
 
+import com.sapp.model.Priority
+import com.sapp.model.Task
 import io.ktor.server.application.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
@@ -17,6 +19,16 @@ fun Application.configureTemplating() {
     routing {
         get("/html-thymeleaf") {
             call.respond(ThymeleafContent("index", mapOf("user" to ThymeleafUser(1, "user1"))))
+        }
+
+        get("/tasks") {
+            val tasks = listOf(
+                Task("cleaning", "Clean the house", Priority.LOW),
+                Task("gardening", "Mow the lawn", Priority.MEDIUM),
+                Task("shopping", "Buy the groceries", Priority.HIGH),
+                Task("painting", "Paint the fence", Priority.MEDIUM)
+            )
+            call.respond(ThymeleafContent("all-tasks", mapOf("tasks" to tasks)))
         }
     }
 }
