f38ac35459c81f7496ca7216392fb3d7bb064e55
Samuel Peña <samuelpena098@gmail.com>
2025-01-11 07:46:17 -0400

    feat: add the task methods and thymeleaf templates to the Templating file

--- src/main/kotlin/com/sapp/Templating.kt
+++ src/main/kotlin/com/sapp/Templating.kt
@@ -2,7 +2,10 @@ package com.sapp
 
 import com.sapp.model.Priority
 import com.sapp.model.Task
+import com.sapp.model.TaskRepository
+import io.ktor.http.*
 import io.ktor.server.application.*
+import io.ktor.server.request.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
 import io.ktor.server.thymeleaf.*
@@ -21,14 +24,88 @@ fun Application.configureTemplating() {
             call.respond(ThymeleafContent("index", mapOf("user" to ThymeleafUser(1, "user1"))))
         }
 
-        get("/tasks") {
-            val tasks = listOf(
-                Task("cleaning", "Clean the house", Priority.LOW),
-                Task("gardening", "Mow the lawn", Priority.MEDIUM),
-                Task("shopping", "Buy the groceries", Priority.HIGH),
-                Task("painting", "Paint the fence", Priority.MEDIUM)
-            )
-            call.respond(ThymeleafContent("all-tasks", mapOf("tasks" to tasks)))
+        route("/tasks") {
+            get {
+                val tasks = TaskRepository.allTasks()
+                call.respond(
+                    ThymeleafContent("all-tasks", mapOf("tasks" to tasks))
+                )
+            }
+
+            get("/byName") {
+                val name = call.request.queryParameters["name"]
+                if (name == null) {
+                    call.respond(HttpStatusCode.BadRequest)
+                    return@get
+                }
+
+                val task = TaskRepository.taskByName(name)
+                if (task == null) {
+                    call.respond(HttpStatusCode.NotFound)
+                    return@get
+                }
+
+                call.respond(
+                    ThymeleafContent("single-task", mapOf("task" to task))
+                )
+            }
+
+            get("/byPriority") {
+                val priorityAsText = call.request.queryParameters["priority"]
+                if (priorityAsText == null) {
+                    call.respond(HttpStatusCode.BadRequest)
+                    return@get
+                }
+
+                try {
+                    val priority = Priority.valueOf(priorityAsText.uppercase())
+                    val tasks = TaskRepository.tasksByPriority(priority)
+
+                    if (tasks.isEmpty()) {
+                        call.respond(HttpStatusCode.NotFound)
+                        return@get
+                    }
+                    val data = mapOf(
+                        "priority" to priority,
+                        "tasks" to tasks
+                    )
+
+                    call.respond(ThymeleafContent("tasks-by-priority", data))
+                } catch (ex: IllegalArgumentException) {
+                    call.respond(HttpStatusCode.BadRequest)
+                }
+            }
+
+            post {
+                val formContent = call.receiveParameters()
+                val params = Triple(
+                    formContent["name"] ?: "",
+                    formContent["description"] ?: "",
+                    formContent["priority"] ?: ""
+                )
+                if (params.toList().any { it.isEmpty() }) {
+                    call.respond(HttpStatusCode.BadRequest)
+                    return@post
+                }
+                try {
+                    val priority = Priority.valueOf(params.third.uppercase())
+                    TaskRepository.addTask(
+                        Task(
+                            params.first,
+                            params.second,
+                            priority
+                        )
+                    )
+                    val tasks = TaskRepository.allTasks()
+                    call.respond(
+                        ThymeleafContent("all-tasks", mapOf("tasks" to tasks))
+                    )
+                } catch (ex: IllegalArgumentException) {
+                    call.respond(HttpStatusCode.BadRequest)
+                } catch (ex: IllegalStateException) {
+                    call.respond(HttpStatusCode.BadRequest)
+                }
+            }
         }
     }
 }
