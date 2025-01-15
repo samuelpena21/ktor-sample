9f6ce9688e3910e9bdf15f2f620533ce9bac4463
Samuel Peña <samuelpena098@gmail.com>
2025-01-15 06:47:03 -0400

    refactor: make repository methods suspend to easily change dispatcher on DB implementation

--- src/main/kotlin/com/sapp/model/FakeTaskRepository.kt
+++ src/main/kotlin/com/sapp/model/FakeTaskRepository.kt
@@ -9,22 +9,22 @@ class FakeTaskRepository : TaskRepository {
         Task("painting", "Paint the fence", Priority.MEDIUM)
     )
 
-    override fun allTasks(): List<Task> = tasks
+    override suspend fun allTasks(): List<Task> = tasks
 
-    override fun tasksByPriority(priority: Priority) = tasks.filter { it.priority == priority }
+    override suspend fun tasksByPriority(priority: Priority) = tasks.filter { it.priority == priority }
 
-    override fun taskByName(name: String) = tasks.find {
+    override suspend fun taskByName(name: String) = tasks.find {
         it.name.equals(name, ignoreCase = true)
     }
 
-    override fun addTask(task: Task) {
+    override suspend fun addTask(task: Task) {
         if(taskByName(task.name) != null) {
             throw IllegalStateException("Cannot duplicate task names!")
         }
         tasks.add(task)
     }
 
-    override fun removeTask(name: String): Boolean {
+    override suspend fun removeTask(name: String): Boolean {
         return tasks.removeIf { it.name == name }
     }
 }
